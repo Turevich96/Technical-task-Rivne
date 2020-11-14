eb70f21041aed0ef0087c970d080cb0654b65bb1
Yura Turevich <oleo.ukraine@gmail.com>
2020-11-14 17:35:17 +0200

    add desktop style

---
+++ dist/js/script.js
@@ -0,0 +1,10 @@
+// slider
+$("#slider").slick({
+  slidesToShow: 3,
+  slidesToScroll: 2,
+  autoplay: true,
+  dots: true,
+  autoplaySpeed: 2000,
+  prevArrow: "<img src='./assets/img/arrow-left.svg' class='prev' alt='1'>",
+  nextArrow: "<img src='./assets/img/arrow-right.svg' class='next' alt='2'>",
+});
