f2218eb575b8e3220bc8992fc915578d9b0e4552
Yura Turevich <oleo.ukraine@gmail.com>
2020-11-14 23:16:26 +0200

    add responsive

--- #src/script/script.js
+++ #src/script/script.js
@@ -8,3 +8,32 @@ $("#slider").slick({
   prevArrow: "<img src='./assets/img/arrow-left.svg' class='prev' alt='1'>",
   nextArrow: "<img src='./assets/img/arrow-right.svg' class='next' alt='2'>",
 });
+
+// Burger Menu
+
+function burgerMenu(selector) {
+  let menu = $(selector);
+  let button = menu.find(".burger-menu_button", ".burger-menu_lines");
+  let links = menu.find(".burger-menu_link");
+  let overlay = menu.find(".burger-menu_overlay");
+
+  button.on("click", (e) => {
+    e.preventDefault();
+    toggleMenu();
+  });
+
+  links.on("click", () => toggleMenu());
+  overlay.on("click", () => toggleMenu());
+
+  function toggleMenu() {
+    menu.toggleClass("burger-menu_active");
+
+    if (menu.hasClass("burger-menu_active")) {
+      $("body").css("overlow", "hidden");
+    } else {
+      $("body").css("overlow", "visible");
+    }
+  }
+}
+
+burgerMenu(".burger-menu");
